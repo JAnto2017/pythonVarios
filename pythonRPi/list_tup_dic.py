e1dbb85afbf0da38b23a83c5fb60678d9a330124
cursooperaciones2017@gmail.com <cursooperaciones2017@gmail.com>
2021-11-02 08:30:52 +0000

    Add: prog libro python en RPi

---
+++ pythonRPi/list_tup_dic.py
@@ -0,0 +1,12 @@
+lis_1 = list([1,2,3])
+lis_2 = [1,'a',list(),dict()]
+print ("Lista. ",lis_1)
+print ("Lista. ",lis_2)
+#--------------------------------- Tuplas
+tup_1 = tuple([1,2,3])
+print ("Tupla: ",tup_1)
+#--------------------------------- Diccionarios
+dic = ({'Sevilla':1,'Madrid':2,'Santander':3})
+print("Diccionario: ",dic)
+print(dic['Sevilla'])
+print(dic[1])
