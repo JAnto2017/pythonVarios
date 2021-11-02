3841045b96ebfc3ce4912510d2da0ee55ed01e80
cursooperaciones2017@gmail.com <cursooperaciones2017@gmail.com>
2021-11-02 08:39:14 +0000

    Add: prog listas-tupla-diccionarios-set

--- pythonRPi/list_tup_dic.py
+++ pythonRPi/list_tup_dic.py
@@ -1,12 +1,22 @@
+#--------------------------------- Listas
 lis_1 = list([1,2,3])
 lis_2 = [1,'a',list(),dict()]
 print ("Lista. ",lis_1)
 print ("Lista. ",lis_2)
-#--------------------------------- Tuplas
+#--------------------------------- Tuplas (son inmutables)
 tup_1 = tuple([1,2,3])
 print ("Tupla: ",tup_1)
-#--------------------------------- Diccionarios
+#--------------------------------- Diccionarios {key:value}
 dic = ({'Sevilla':1,'Madrid':2,'Santander':3})
 print("Diccionario: ",dic)
 print(dic['Sevilla'])
-print(dic[1])
+#print(dic[1]) este causa error
+print(dic.keys())
+print(dic.values())
+#--------------------------------- Set {para valores únicos}
+set_1 = {1,2,3,4,5}
+print("Set con valores únicos: ",set_1)
+lista = [1,2,3,4,5,6,7,8,8,4,9]
+print("Lista con valores repetidos: ",lista)
+set_2 = set(lista)
+print("Conversión a set: ",set_2)
